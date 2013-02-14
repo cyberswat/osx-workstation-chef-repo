ff460ff15db14ad9c5064f721e1df8ba34103351
Kevin Bridges <kevin.bridges@acquia.com>
2013-02-13 21:55:35 -0700

    Adding virtualbox attributes

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -10,5 +10,8 @@ default_attributes(
     "version" => "_v1_0_0",
     "checksum" => "2afad022b1e1f08b3ed40f0c2bde7bf7cce003852c83f85948c7f57a5578d9c5",
     "tmux_enabled" => false,
+  },
+  "virtualbox" => {
+    "url" => "http://download.virtualbox.org/virtualbox/4.2.6/VirtualBox-4.2.6-82870-OSX.dmg",
   }
 )
